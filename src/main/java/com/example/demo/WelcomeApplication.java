80f437baadbe3ce745cb0f58a9c311f6ad70198c
sanuppala <shanthaprasad@gmail.com>
2018-08-10 09:52:44 +0530

    Primary

---
+++ src/main/java/com/example/demo/WelcomeApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeApplication.class, args);
+	}
+}
